e49dfc5522e541723aad2bc66e38451f0790e7ec
rankec <rankec2221@gmail.com>
2018-06-30 13:03:34 +0200

    spring initializer

---
+++ server/src/main/java/de/rankec/uptimecheck/UptimecheckApplication.java
@@ -0,0 +1,12 @@
+package de.rankec.uptimecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimecheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimecheckApplication.class, args);
+	}
+}
---
+++ server/src/test/java/de/rankec/uptimecheck/UptimecheckApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rankec.uptimecheck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UptimecheckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
